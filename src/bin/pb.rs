6b2af8e68b1033aba038fca80a0b9f08f8792ef9
Felix Zwettler <f.zwettler@posteo.de>
2026-09-23 20:05:13 +0200

    feat: re-implement using guillotine UI framework

--- src/bin/pb.rs
+++ src/bin/pb.rs
@@ -1,23 +1,16 @@
 use core::time::Duration;
-use embedded_graphics::image::ImageRaw;
-use embedded_graphics::mono_font::MonoTextStyle;
-use embedded_graphics::mono_font::ascii::FONT_10X20;
-use embedded_graphics::pixelcolor::raw::BigEndian;
 use embedded_graphics::prelude::*;
-use embedded_graphics::primitives::{PrimitiveStyleBuilder, StyledDrawable};
-use embedded_graphics::text::{Alignment, Text};
 use embedded_graphics_core::pixelcolor::Gray8;
+use guillotine::{DirectTarget, FrameStorage, Ui};
 use inkview::event::Key;
 use inkview_eg::InkviewDisplay;
 use pb_covers::state::State;
-use std::cell::OnceCell;
-use std::convert::Infallible;
+use pb_covers::ui::CoverImageNode;
 use std::path::PathBuf;
 use std::sync::{LazyLock, mpsc};
 use std::{env, thread};
 
-static DEFAULT_SEARCH_PATH: LazyLock<PathBuf> =
-    LazyLock::new(|| PathBuf::from("/mnt/ext1/Literature"));
+static DEFAULT_SEARCH_PATH: &str = "/mnt/ext1/Literature";
 
 #[derive(Debug, Clone)]
 enum CoversEvent {
@@ -32,15 +25,26 @@ fn main() -> anyhow::Result<()> {
     let search_dir = env::args()
         .nth(1)
         .map(PathBuf::from)
-        .unwrap_or(DEFAULT_SEARCH_PATH.clone());
+        .unwrap_or(PathBuf::from(DEFAULT_SEARCH_PATH));
     let cycle_duration: Duration = env::args()
         .nth(2)
         .map(|s| Duration::from_secs(s.parse::<u64>().expect("Not a valid number")))
-        .unwrap_or(Duration::from_secs(60));
-    let mut state = State::new(search_dir);
+        .unwrap_or(Duration::from_secs(600));
 
     thread::spawn(move || {
-        let mut display = OnceCell::new();
+        let mut state = State::new(search_dir, Size::zero());
+        let mut ui = LazyLock::new(|| {
+            let mut display = InkviewDisplay::new(iv);
+            display.clear(Gray8::WHITE);
+            display.flush();
+            let storage = FrameStorage::<Gray8, 64, 1024, CoverImageNode>::default();
+            let theme = guillotine::Theme {
+                background: Gray8::WHITE,
+                foreground: Gray8::BLACK,
+            };
+
+            Ui::with_theme(DirectTarget::new(display), storage, theme)
+        });
 
         loop {
             let mut render = false;
@@ -51,13 +55,11 @@ fn main() -> anyhow::Result<()> {
                     break;
                 }
             };
+            eprintln!("Received inkview event {event:?}");
             match event {
                 CoversEvent::InkviewEvent(inkview::Event::Init) => {
-                    // Create a new inkview display which implements [embedded_graphics_core::DrawTarget]
-                    let mut iv_display = InkviewDisplay::new(iv);
-                    iv_display.clear(Gray8::WHITE);
-                    iv_display.flush();
-                    let _ = display.set(iv_display);
+                    let ui = LazyLock::force(&mut ui);
+                    state.update_screen_size(ui.display().size());
                     render = true;
                 }
                 CoversEvent::InkviewEvent(inkview::Event::Show)
@@ -75,7 +77,7 @@ fn main() -> anyhow::Result<()> {
                     render = true;
                 }
                 CoversEvent::InkviewEvent(inkview::Event::KeyDown { key: Key::Menu }) => {
-                    state.toggle_overlay();
+                    state.toggle_info();
                     render = true;
                 }
                 CoversEvent::InkviewEvent(inkview::Event::KeyDown { .. })
@@ -86,12 +88,11 @@ fn main() -> anyhow::Result<()> {
                 }
                 _ => {}
             }
-
-            if render && let Some(display) = display.get_mut() {
-                if let Err(err) = draw_content(&mut state, display) {
-                    eprintln!("Unable to draw content: {err:?}");
-                };
-                display.flush()
+            #[allow(clippy::collapsible_if)]
+            if render && let Some(ui) = LazyLock::get_mut(&mut ui) {
+                // Infallible.
+                let _ = ui.render(&state);
+                ui.display_mut().flush();
             }
         }
 
@@ -116,56 +117,6 @@ fn main() -> anyhow::Result<()> {
         }
         Some(())
     });
-
-    Ok(())
-}
-
-fn draw_content(
-    state: &mut State,
-    display: &mut impl DrawTarget<Color = Gray8, Error = Infallible>,
-) -> anyhow::Result<()> {
-    let display_size = display.bounding_box().size;
-    let text_style = MonoTextStyle::new(&FONT_10X20, Gray8::new(0x00));
-    let text_bg_style = PrimitiveStyleBuilder::new()
-        .stroke_color(Gray8::BLACK)
-        .fill_color(Gray8::WHITE)
-        .build();
-
-    if let Some(buf) = state.cover_image_buf(display_size) {
-        // draw the cover image..
-        let image = ImageRaw::<Gray8, BigEndian>::new(buf.as_raw(), buf.width());
-        image.draw(display);
-    } else {
-        // ..or a placeholder text
-        display.clear(Gray8::WHITE);
-        let text = "- no cover found -";
-        Text::with_alignment(
-            text,
-            display.bounding_box().center(),
-            text_style,
-            Alignment::Center,
-        )
-        .draw(display)?;
-    };
-
-    if state.overlay() {
-        let text_str = format!(
-            "pos: {}, book: {}",
-            state.pos(),
-            state
-                .current_book()
-                .and_then(|p| p.file_name().map(|s| s.to_string_lossy().to_string()))
-                .unwrap_or("<unavailable>".to_string())
-        );
-        let text = Text::with_alignment(
-            &text_str,
-            display.bounding_box().top_left + Point::new(20, 20),
-            text_style,
-            Alignment::Left,
-        );
-        text.bounding_box().draw_styled(&text_bg_style, display)?;
-        text.draw(display)?;
-    }
 
     Ok(())
 }
